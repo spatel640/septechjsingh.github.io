68ec37baf11f3428635a2d5ab65f77d04e744e08
jsingh <jsingh>
2016-08-15 16:09:02 -0400

    organization

--- testPortalScripts.js
+++ testPortalScripts.js
@@ -7,21 +7,52 @@ function verifyAddress() {
     street = encodeURIComponent(street.trim());
     borough = encodeURIComponent(borough.trim());
 
-    alert("Street Name: " + street);
-    alert("Borough Name: " + borough);
+    $.ajax({
+        url: "https://api.cityofnewyork.us/geoclient/v1/address.json?houseNumber=" + number + "&street=" + street + "&borough=" + borough + "&app_id=b519ff0c&app_key=8351acab95743febeb729768d1251777",
+        dataType: "jsonp",
+        jsonpCallback: "logResults"
+    });
+}
 
-    var settings = {
-        "async": false,
-        "crossDomain": false,
-        "url": "https://api.cityofnewyork.us/geoclient/v1/address.json?houseNumber=321&street=Union%20Avenue&borough=Staten%20Island&app_id=b519ff0c&app_key=8351acab95743febeb729768d1251777",
-        "method": "GET",
-        "headers": {
-            "cache-control": "no-cache",
-            "postman-token": "208b742b-65c0-60e9-6277-e1036f455892"
+function logResults(json) {
+    console.log(json);
+    var returnCode = json.address.geosupportReturnCode;
+    if (returnCode != "EE" && returnCode != "16") {
+        document.getElementById("correct").style.display = "block";
+        document.getElementById("correct_address").style.display = "block";
+        document.getElementById("address_choices").style.display = "none";
+
+        var houseNumber = json.address.houseNumber;
+        var street = json.address.firstStreetNameNormalized;
+        var borough = json.address.uspsPreferredCityName;
+        var zip = json.address.zipCode;
+
+        document.getElementById("correct_number").innerHTML = houseNumber;
+        document.getElementById("correct_street").innerHTML = street;
+        document.getElementById("correct_borough").innerHTML = borough;
+        document.getElementById("zip_code").innerHTML = zip;
+
+        document.getElementById("correct_number").style.display = "block";
+        document.getElementById("correct_street").style.display = "block";
+        document.getElementById("correct_borough").style.display = "block";
+        document.getElementById("zip_code").style.display = "block";
+    } else {
+        document.getElementById("correct").style.display = "none";
+        document.getElementById("correct_address").style.display = "none"
+        document.getElementById("address_choices").style.display = "block";
+        var numSimilar = Number(json.address.numberOfStreetCodesAndNamesInList);
+
+        if (numSimilar >= 3) {
+            var choice1 = json.address.streetName1;
+            var choice2 = json.address.streetName2;
+            var choice3 = json.address.streetName3;
+
+            document.getElementById("address1").innerHTML = choice1;
+            document.getElementById("address2").innerHTML = choice2;
+            document.getElementById("address3").innerHTML = choice3;
+            document.getElementById("add1").value = choice1;
+            document.getElementById("add2").value = choice2;
+            document.getElementById("add3").value = choice3;
         }
     }
-
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-    });
 }
