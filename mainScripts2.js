c7a3eee3971a3579087f4c153b0e3b415866c67e
jsingh <jsingh>
2016-09-09 09:59:54 -0400

    auth updates

--- mainScripts2.js
+++ mainScripts2.js
@@ -9,7 +9,7 @@ function getDates() {
     var d = new Date();
     var n = d.getHours();
 
-    if (n >= 12) {
+    if (n >= 20) {
         now.add(1, 'days');
     }
 
@@ -19,9 +19,9 @@ function getDates() {
     var district = document.getElementById("district").value;
     console.log(district);
 
-    var myUrl1 = "https://apis.accela.com/v4/inspections/availableDates?typeId=5&recordId=PARTNER-16CAP-00000-0002O&startDate=" + date2Pass + "&validateScheduleNumOfDays=true&validateCutOffTime=true";
-    var myUrl2 = "https://apis.accela.com/v4/inspections/availableDates?typeId=5&recordId=PARTNER-16CAP-00000-0002P&startDate=" + date2Pass + "&validateScheduleNumOfDays=true&validateCutOffTime=true";
-    var myUrl3 = "https://apis.accela.com/v4/inspections/availableDates?typeId=5&recordId=PARTNER-16CAP-00000-0002Q&startDate=" + date2Pass + "&validateScheduleNumOfDays=true&validateCutOffTime=true";
+    var myUrl1 = "https://apis.accela.com/v4/inspections/availableDates?typeId=5&recordId=PARTNER-16CAP-00000-0002O&startDate=" + date2Pass + "&validateScheduleNumOfDays=true&validateCutOffTime=false";
+    var myUrl2 = "https://apis.accela.com/v4/inspections/availableDates?typeId=5&recordId=PARTNER-16CAP-00000-0002P&startDate=" + date2Pass + "&validateScheduleNumOfDays=true&validateCutOffTime=false";
+    var myUrl3 = "https://apis.accela.com/v4/inspections/availableDates?typeId=5&recordId=PARTNER-16CAP-00000-0002Q&startDate=" + date2Pass + "&validateScheduleNumOfDays=true&validateCutOffTime=false";
 
     var myUrl = "";
 
