8d86d17918ed300e32e7570a0c7012985f5e6c05
jsingh <jsingh>
2016-09-14 18:08:24 -0400

    updates

--- field_agent.js
+++ field_agent.js
@@ -1,3 +1,5 @@
+var authorization_token = "YiUfCb7NeS8o7bxCfLHDqDaa0JMlzFyuhBEC1LlZIz28ZPRIjEsNktv07vf3Xms7_CJrQoRhFmvIM4Honad0akmpg3AK-SFzwLzWrV6LTx5o35lk_JHI1AxrPZF5jVO-lSu6b1XdTB_yYqVe4wRQNq8XFKFUH6JU4wqbiIjmxrnbbF-R5uXeM5vqdYNLJegduM5_4_i9Lu369aNFBgbbal-QC0ld2wRtfc-t5inDRp0qAwVpwsLAbOqTSBfBasXI_AW4YxS5OCJFnXHDY2QJPObmEFAiueMepOk47QzQNiEZ3zae95cVMXxOqluCvsYd50PevSzZRFergzFB0TWjO3m16uLibSbVGsEYMXJlsNsmMAKrG6PV3WNNLorYdBN3gImwEfwdG_X-AhDIHqEBRPZL_CwHBh6pPQZn-PNYaGDdwu93EP5NcsI0ymRDlGT7W_UCyGWBUCFd0uSWdws-iblMpu7wxQlbuhn45SFBxRE1";
+
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -20,15 +22,12 @@ function getCookie(cname) {
     return "";
 }
 
-function checkCookie() {
-    var user = getCookie("username");
+function checkCookie(cookie2check) {
+    var user = getCookie(cookie2check);
     if (user != "") {
-        alert("Welcome again " + user);
+        return true;
     } else {
-        user = prompt("Please enter your name:", "");
-        if (user != "" && user != null) {
-            setCookie("username", user, 365);
-        }
+        return false;
     }
 }
 
@@ -45,5 +44,106 @@ function redirector() {
         if ($('#rememberMe').val() == true)
             setCookie('remembered', username, 1);
         window.location.href = '/field_agent_P2.html';
+    }
+}
+
+function getJobs() {
+    $('.panel').hide();
+    var myInspections;
+    if (checkCookie("username")) {
+        var username = getCookie("username");
+        console.log(username);
+
+        var now = moment();
+        now.add(1, 'days');
+        var temp = now.format('YYYY MM DD').toString();
+        var date2Pass = temp[0] + temp[1] + temp[2] + temp[3] + '-' + temp[5] + temp[6] + '-' + temp[8] + temp[9];
+
+        var settings = {
+            "async": false,
+            "crossDomain": true,
+            "url": "https://apis.accela.com/v4/inspections?inspectorIds=" + username + "&scheduledDateFrom=" + date2Pass + "&scheduledDateTo=" + date2Pass,
+            "method": "GET",
+            "headers": {
+                "authorization": authorization_token,
+                "cache-control": "no-cache",
+                "postman-token": "52db0691-8db4-6938-7922-89ec9593e4da"
+            }
+        }
+
+        var i;
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            if (response.result != null && response.result != undefined && response.result != "") {
+                myInspections = new Array(response.result.length);
+                for (i = 0; i < response.result.length; i++) {
+                    var capId = response.result[i].recordId.id;
+                    var custId = response.result[i].recordId.customId;
+                    var bldg = response.result[i].address.streetStart;
+                    var street = response.result[i].address.streetName;
+                    var lat = response.result[i].address.xCoordinate;
+                    var lon = response.result[i].address.yCoordinate;
+                    var zipCode = response.result[i].address.postalCode;
+                    var district = response.result[i].address.inspectionDistrict;
+                    var secondary = response.result[i].address.secondaryStreet;
+                    var id = response.result[i].id;
+                    var district = 501;
+
+                    if (username.toUpperCase() == "SI01")
+                        district = 501;
+                    else if (username.toUpperCase() == "SI02")
+                        district = 502;
+                    else
+                        district = 503;
+
+                    var name = '';
+
+                    var settings = {
+                        "async": false,
+                        "crossDomain": true,
+                        "url": "https://apis.accela.com/v4/records/" + capId,
+                        "method": "GET",
+                        "headers": {
+                            "authorization": authorization_token,
+                            "cache-control": "no-cache",
+                            "postman-token": "59acabbe-f19d-c8a1-f10d-dd1b1918b660"
+                        }
+                    }
+
+                    $.ajax(settings).done(function (response) {
+                        console.log(response);
+                        name = response.result[0].name;
+                    });
+
+                    var myInspection = new Insepction(capId, custId, district, name, bldg, street, lat, lon, zipCode, district, secondary, id);
+                    myInspections[i] = myInspection;
+                }
+            }
+        });
+
+        if (myInspections != undefined) {
+            for (i; i < myInspections.length; i++) {
+                myInspections[i] = null;
+            }
+            for (i = 0; i < myInspections.length; i++) {
+                var cur_num = (i + 1).toString();
+                document.getElementById('altId' + cur_num).innerHTML = myInspections[i].altId;
+                document.getElementById('content' + cur_num).innerHTML = myInspections[i].name;
+                document.getElementById('bldg' + cur_num).innerHTML = myInspections[i].bldg;
+                document.getElementById('street' + cur_num).innerHTML = myInspections[i].street;
+                document.getElementById('cross' + cur_num).innerHTML = myInspections[i].secondary;
+                document.getElementById('district' + cur_num).innerHTML = myInspections[i].district;
+                document.getElementById('cap' + cur_num).value = myInspections[i].capId;
+                document.getElementById('lat' + cur_num).value = myInspections[i].lat;
+                document.getElementById('lon' + cur_num).value = myInspections[i].lon;
+                document.getElementById('insp' + cur_num).value = myInspections[i].inspectionId;
+                document.getElementById('panel' + cur_num).style.display = 'block';
+            }
+        }
+        console.log(myInspections);
+
+    } else {
+        console.log("Username did not exist");
     }
 }
---
+++ inspection.js
@@ -0,0 +1,15 @@
+var Insepction = function (capId, altId, district, name, bldg, street, lat, lon, zipCode, district, secondary, id) {
+    this.capId = capId;
+    this.altId = altId;
+    this.district = district;
+    this.name = name;
+
+    this.bldg = bldg;
+    this.street = street;
+    this.lat = lat;
+    this.lon = lon;
+    this.zipCode = zipCode;
+    this.secondary = secondary;
+
+    this.inspectionId = id;
+}
